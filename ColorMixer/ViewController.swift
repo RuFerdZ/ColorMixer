ce27f3194d9ba1764d3f6fa230c62d582396036d
RuFerdZ <ge.rusiru@gmail.com>
2022-03-11 03:55:14 +0800

    added color mixer project

--- ColorMixer/ViewController.swift
+++ ColorMixer/ViewController.swift
@@ -8,12 +8,40 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    var red = 0.0
+    var green = 0.0
+    var blue = 0.0
+    
+    @IBOutlet weak var colorValue: UILabel!
+    
+    @IBOutlet weak var background: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        background.backgroundColor = UIColor(red: red/255.0, green: green/255.0, blue: blue/255.0, alpha: 1.0)
+        colorValue.text = "\(Int(red)), \(Int(green)), \(Int(blue))"
     }
 
-
+    @IBAction func redSlider(_ sender: UISlider) {
+        red = CGFloat(sender.value)
+        background.backgroundColor = UIColor(red: red/255.0, green: green/255.0, blue: blue/255.0, alpha: 1.0)
+        colorValue.text = "\(Int(red)), \(Int(green)), \(Int(blue))"
+    }
+    
+    @IBAction func greenSlider(_ sender: UISlider) {
+        green = CGFloat(sender.value)
+        background.backgroundColor = UIColor(red: red/255.0, green: green/255.0, blue: blue/255.0, alpha: 1.0)
+        colorValue.text = "\(Int(red)), \(Int(green)), \(Int(blue))"
+        
+    }
+    
+    @IBAction func blueSlider(_ sender: UISlider) {
+        blue = CGFloat(sender.value)
+        background.backgroundColor = UIColor(red: red/255.0, green: green/255.0, blue: blue/255.0, alpha: 1.0)
+        colorValue.text = "\(Int(red)), \(Int(green)), \(Int(blue))"
+    }
 }
 
